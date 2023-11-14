1d25c325920bcb3b407c571c53ac0ff7d0d50921
Suslek <TTe4eHbKe@gmail.com>
2023-11-15 00:12:34 +0800

    changed:     UseDaoJDBCImpl:         try-with-resources instead of try-catch         PreparedStatement instead of Statement in not DDL statements     UserServiceImpl:         field privatized added:     User:         equals & hashCode

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,32 +3,18 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-
-    private final Connection connection = Util.getMySQLConnection();
-    private Statement statement;
-
-    {
-        try {
-            statement = connection.createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try {
+        try (Connection connection = Util.getMySQLConnection(); Statement statement = connection.createStatement()) {
             statement.executeUpdate("CREATE TABLE USERS (" +
                     "id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
                     "name VARCHAR(255), " +
@@ -39,31 +25,38 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        try {
+        try (Connection connection = Util.getMySQLConnection(); Statement statement = connection.createStatement()) {
             statement.executeUpdate("DROP TABLE USERS");
         } catch (SQLException ignored) {
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            statement.executeUpdate("INSERT INTO USERS (name, lastname, age)" +
-                    "VALUES (\"" + name + "\",\"" + lastName + "\" ,\"" + age + "\")");
+        try (Connection connection = Util.getMySQLConnection(); PreparedStatement preparedStatement =
+                connection.prepareStatement("INSERT INTO USERS (name, lastName, age) Values (?,?,?)");
+        ) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
         } catch (SQLException ignored) {
         }
     }
 
     public void removeUserById(long id) {
-        try {
-            statement.executeUpdate("DELETE from USER where id =" + id);
+        try (Connection connection = Util.getMySQLConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("DELETE from USER where id = ?")) {
+            preparedStatement.setLong(1, id);
         } catch (SQLException ignored) {
         }
     }
 
     public List<User> getAllUsers() {
         List<User> result = new ArrayList<>();
-        try {
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM test.users");
+        try (Connection connection = Util.getMySQLConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM test.users")
+        ) {
+            ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 result.add(new User(resultSet.getString("name"),
                         resultSet.getString("lastName"),resultSet.getByte("Age")));
@@ -77,7 +70,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try {
+        try (Connection connection = Util.getMySQLConnection(); Statement statement = connection.createStatement()) {
             statement.executeUpdate("TRUNCATE TABLE USERS");
         } catch (SQLException e) {
             throw new RuntimeException(e);
